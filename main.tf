0b9cf19e6e98b8b6c1b3e94bfed7cb9ddc00d371
bharti-12378 <bharti.ghildiyal@ankercloud.com>
2025-02-13 22:09:16 +0530

    second

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket = "terraform-backend-bucket"
-    key    = "terraform/state"
+    bucket = "terraform-backend-bucket-sample"
+    key    = "terraform/statefile"
     region = "us-east-1"
   }
 }
